6d60708662f1158c952d154bceb7cf0b87068762
Nadeem EDOO <nadeem.edooetu@gmail.com>
2015-04-18 18:06:11 +0200

    Ajout Fichier de test contenant le main " T_Pge_Acc" pour dossier "PageAcc"

---
+++ src/PageAcc/T_PgeAcc.java
@@ -0,0 +1,18 @@
+package PageAcc;
+//import javax.swing.JFrame;
+
+
+/* NE PAS UTILISER DE CLASSE QUI EXTENDS UN JFRAME
+ * POUR NE PAS MODIFIER LE COMPORTEMENT DU JFRAME
+ * A VERIFIER
+ * 
+ */
+public class T_PgeAcc {
+	
+	public static void main(String [] args){
+		
+		//PageAccueil FenAcc = new PageAccueil();
+		MyFrame Accueil = new MyFrame();
+		Accueil.setVisible(true);
+	}
+}
